5cff2e186b8c0beb81bacbfdcf2a9f670d21960b
tbtraltaa <tbtr_altaa@yahoo.com>
2012-04-13 13:54:23 +0800

    test

--- lookup.py
+++ lookup.py
@@ -16,10 +16,6 @@ def add_to_index(index, keyword, url):
     # not found, add new keyword to index
     index.append([keyword, [url]])
 
-
-
-
-
 def get_page(url):
     try:
         if url == "http://www.udacity.com/cs101x/index.html":
